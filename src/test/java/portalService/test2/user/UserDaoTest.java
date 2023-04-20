80f2b4ef03e38762f41f7c85bff05c4da46a515b
endyd1567 <endyd1567@gmail.com>
2023-04-20 14:45:04 +0900

    2. 두 회사 납품

--- src/test/java/portalService/test2/user/UserDaoTest.java
+++ src/test/java/portalService/test2/user/UserDaoTest.java
@@ -15,7 +15,7 @@ class UserDaoTest {
         String name = "umdu";
         String password = "1234";
 
-        UserDao userDao = new UserDao();
+        UserDao userDao = new JejuUserDao();
         User user = userDao.findById(1l);
 
         assertThat(user.getId()).isEqualTo(id);
@@ -33,7 +33,42 @@ class UserDaoTest {
         user.setName(name);
         user.setPassword(password);
 
-        UserDao userDao = new UserDao();
+        UserDao userDao = new JejuUserDao();
+        userDao.insert(user);
+        User insertedUser = userDao.findById(user.getId());
+
+        assertThat(insertedUser.getId()).isEqualTo(user.getId());
+        assertThat(insertedUser.getId()).isGreaterThan(1l);
+        assertThat(insertedUser.getName()).isEqualTo(user.getName());
+        assertThat(insertedUser.getPassword()).isEqualTo(user.getPassword());
+    }
+
+    @Test
+    public void getForHalla() throws SQLException, SQLException {
+
+        Long id = 1l;
+        String name = "umdu";
+        String password = "1234";
+
+        UserDao userDao = new HallaUserDao();
+        User user = userDao.findById(1l);
+
+        assertThat(user.getId()).isEqualTo(id);
+        assertThat(user.getName()).isEqualTo(name);
+        assertThat(user.getPassword()).isEqualTo(password);
+
+    }
+
+    @Test
+    public void insertForHalla() throws SQLException {
+
+        User user = new User();
+        String name = "엄두용";
+        String password = "1234";
+        user.setName(name);
+        user.setPassword(password);
+
+        UserDao userDao = new HallaUserDao();
         userDao.insert(user);
         User insertedUser = userDao.findById(user.getId());
 
